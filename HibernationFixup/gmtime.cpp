7657b0caff0d37899fb6ad9720f090f1b525b092
amane1234 <47608969+amane1234@users.noreply.github.com>
2023-09-22 13:07:00 -0400

    Update gmtime.cpp

--- HibernationFixup/gmtime.cpp
+++ HibernationFixup/gmtime.cpp
@@ -1,9 +1,5 @@
-//
-//  gmtime.cpp
-//  HibernationFixup
-//
-//  Copyright © 2019 lvs1974. All rights reserved.
-//
+// gmtime.cpp
+// HibernationFixup
 
 #include <stdint.h>
 #include <sys/time.h>
@@ -11,50 +7,55 @@
 
 #include "gmtime.h"
 
-bool isLeapYear(int year)
-{
-	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+// Function to check if a year is a leap year
+bool isLeapYear(int year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
-int daysInYear(int year)
-{
-	return isLeapYear(year) ? 366 : 365;
+// Function to get the number of days in a year
+int daysInYear(int year) {
+    return isLeapYear(year) ? 366 : 365;
 }
 
-int daysInMonth(int month, int year)
-{
-	static int days[] = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	if ((month == 2) && (daysInYear(year) == 366)) return 29;
-	else return days[month];
-}
-
-int reduceDaysToYear(time_t &days) {
-	int year;
-	for (year = 1970; days > daysInYear(year); year++) {
-		days -= daysInYear(year);
-	}
-	return year;
-}
-
-int reduceDaysToMonths(time_t &days, int year) {
-	int month;
-	for (month = 0; days > daysInMonth(month,year); month++)
-		days -= daysInMonth(month,year);
-	return month;
-}
-
-struct tm * gmtime_r(time_t timer, struct tm * timeptr)
-{
-	timeptr->tm_sec = timer % 60;
-	timer /= 60;
-	timeptr->tm_min = timer % 60;
-	timer /= 60;
-	timeptr->tm_hour = timer % 24;
-	timer /= 24;
+// Function to get the number of days in a specific month of a year
+int daysInMonth(int month, int year) {
+    static int days[] = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	timeptr->tm_year  = reduceDaysToYear(timer);
-	timeptr->tm_mon   = reduceDaysToMonths(timer, timeptr->tm_year);
-	timeptr->tm_mday  = int(timer);
-	
-	return (timeptr);
+    if (month == 2 && isLeapYear(year)) {
+        return 29;
+    }
+    return days[month];
+}
+
+// Function to reduce days to the year and update the year parameter
+int reduceDaysToYear(time_t &days, int &year) {
+    for (year = 1970; days >= daysInYear(year); year++) {
+        days -= daysInYear(year);
+    }
+    return year;
+}
+
+// Function to reduce days to months and update the month parameter
+int reduceDaysToMonths(time_t &days, int year, int &month) {
+    for (month = 0; days >= daysInMonth(month, year); month++) {
+        days -= daysInMonth(month, year);
+    }
+    return month;
+}
+
+// Function to convert time in seconds since epoch to a broken-down time structure
+struct tm *gmtime_r(time_t timer, struct tm *timeptr) {
+    timeptr->tm_sec = timer % 60;
+    timer /= 60;
+    timeptr->tm_min = timer % 60;
+    timer /= 60;
+    timeptr->tm_hour = timer % 24;
+    timer /= 24;
+
+    int year, month;
+    timeptr->tm_year = reduceDaysToYear(timer, year);
+    timeptr->tm_mon = reduceDaysToMonths(timer, year, month);
+    timeptr->tm_mday = static_cast<int>(timer);
+
+    return timeptr;
 }
