9bd4d243c7e3b7d70d86e70971b6172dcdd8cdae
amane1234 <47608969+amane1234@users.noreply.github.com>
2023-09-22 13:10:02 -0400

    Update kern_config.hpp

--- HibernationFixup/kern_config.hpp
+++ HibernationFixup/kern_config.hpp
@@ -1,9 +1,6 @@
-//
-//  kern_config.hpp
-//  HibernationFixup
-//
-//  Copyright © 2016-2017 lvs1974. All rights reserved.
-//
+// kern_config.hpp
+// HibernationFixup
+// Copyright © 2016-2017 lvs1974. All rights reserved.
 
 #ifndef kern_config_private_h
 #define kern_config_private_h
@@ -12,72 +9,51 @@
 
 class Configuration {
 public:
-	/**
-	 *  Possible boot arguments
-	 */
-	static const char *bootargOff[];
-	static const char *bootargDebug[];
-	static const char *bootargBeta[];
-	static constexpr const char *bootargDumpNvram         {"-hbfx-dump-nvram"};          // write NVRAM to file
-	static constexpr const char *bootargDisablePatchPCI   {"-hbfx-disable-patch-pci"};   // disable patch pci family
-	static constexpr const char *bootargPatchPCIWithList  {"hbfx-patch-pci"};            // patch pci family ignored device list
-	static constexpr const char *bootargAutoHibernateMode {"hbfx-ahbm"};                 // auto hibernate mode
+    // Possible boot arguments
+    static const char *bootargOff[];
+    static const char *bootargDebug[];
+    static const char *bootargBeta[];
+    static constexpr const char *bootargDumpNvram         {"-hbfx-dump-nvram"};          // Write NVRAM to file
+    static constexpr const char *bootargDisablePatchPCI   {"-hbfx-disable-patch-pci"};   // Disable patch PCI family
+    static constexpr const char *bootargPatchPCIWithList  {"hbfx-patch-pci"};            // Patch PCI family ignored device list
+    static constexpr const char *bootargAutoHibernateMode {"hbfx-ahbm"};                 // Auto hibernate mode
 
 public:
-	/**
-	 *  Retrieve boot arguments
-	 */
-	void readArguments();
+    // Retrieve boot arguments
+    void readArguments();
 
-	/**
-	 *  dump nvram to /nvram.plist
-	 */
-	bool dumpNvram {false};
+    // Dump NVRAM to /nvram.plist
+    bool dumpNvram {false};
 
-	/**
-	 *  patch PCI Family
-	 */
-	bool patchPCIFamily {true};
+    // Patch PCI Family
+    bool patchPCIFamily {true};
 
-	/**
-	 *  device list (can be separated by comma, space or something like that)
-	 */
-	char ignored_device_list[64] {};
-	
-	/* Flags used to control automatic hibernation behavior (takes place only when system goes to sleep)
-	 */
-	enum AutoHibernateModes {
-		// If this flag is set, system will hibernate instead of regular sleep (flags below can be used to limit this behavior)
-		EnableAutoHibernation           = 1,
-		// Auto hibernation can happen when lid is closed (if bit is not set - no matter which status lid has)
-		WhenLidIsClosed                 = 2,
-		// Auto hibernation can happen when external power is disconnected (if bit is not set - no matter whether it is connected)
-		WhenExternalPowerIsDisconnected = 4,
-		// Auto hibernation can happen when battery is not charging (if bit is not set - no matter whether it is charging)
-		WhenBatteryIsNotCharging        = 8,
-		// Auto hibernation can happen when battery is at warning level (osx is responsible for this level)
-		WhenBatteryIsAtWarnLevel        = 16,
-		// Auto hibernation can happen when battery is at critical level (osx is responsible for this level)
-		WhenBatteryAtCriticalLevel      = 32,
-		// Do not alter next wake up time
-		DoNotOverrideWakeUpTime         = 64,
-		// Disable power event kStimulusDarkWakeActivityTickle in kernel, so this event cannot be a trigger for switching from dark wake to full wake
-		DisableStimulusDarkWakeActivityTickle = 128,
-		
-		// Next 4 bits are used to specify minimal capacity percent remaining value when hibernation will be forced.
-		// Can be used together with WhenBatteryIsAtWarnLevel or WhenBatteryAtCriticalLevel, when IOPMPowerSource cannot detect
-		// warning or critical battery level
-		RemainCapacityBit1				= 256,
-		RemainCapacityBit2				= 512,
-		RemainCapacityBit3				= 1024,
-		RemainCapacityBit4				= 2048
-	};
-	
-	int autoHibernateMode {0};
+    // Device list (can be separated by comma, space, or something similar)
+    char ignored_device_list[64] {};
 
-	Configuration() = default;
+    // Flags used to control automatic hibernation behavior (takes place only when the system goes to sleep)
+    enum AutoHibernateModes {
+        EnableAutoHibernation           = 1,
+        WhenLidIsClosed                 = 2,
+        WhenExternalPowerIsDisconnected = 4,
+        WhenBatteryIsNotCharging        = 8,
+        WhenBatteryIsAtWarnLevel        = 16,
+        WhenBatteryAtCriticalLevel      = 32,
+        DoNotOverrideWakeUpTime         = 64,
+        DisableStimulusDarkWakeActivityTickle = 128,
+
+        // Next 4 bits are used to specify minimal capacity percent remaining value when hibernation will be forced.
+        RemainCapacityBit1				= 256,
+        RemainCapacityBit2				= 512,
+        RemainCapacityBit3				= 1024,
+        RemainCapacityBit4				= 2048
+    };
+
+    int autoHibernateMode {0};
+
+    Configuration() = default;
 };
 
 extern Configuration ADDPR(hbfx_config);
 
-#endif /* kern_config_private_h */
+#endif
