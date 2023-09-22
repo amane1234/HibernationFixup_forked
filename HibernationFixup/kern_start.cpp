9cb4f2dc408d4c162af27cd200caa7bf51c91fbb
amane1234 <47608969+amane1234@users.noreply.github.com>
2023-09-22 13:16:50 -0400

    Update kern_start.cpp

--- HibernationFixup/kern_start.cpp
+++ HibernationFixup/kern_start.cpp
@@ -14,65 +14,65 @@
 static HBFX hbfx;
 
 const char *Configuration::bootargOff[] {
-	"-hbfxoff"
+    "-hbfxoff"
 };
 
 const char *Configuration::bootargDebug[] {
-	"-hbfxdbg"
+    "-hbfxdbg"
 };
 
 const char *Configuration::bootargBeta[] {
-	"-hbfxbeta"
+    "-hbfxbeta"
 };
 
 Configuration ADDPR(hbfx_config);
 
 void Configuration::readArguments() {
-	dumpNvram = checkKernelArgument(bootargDumpNvram);
+    dumpNvram = checkKernelArgument(bootargDumpNvram);
 
-	if (getKernelVersion() >= KernelVersion::Sierra)
-	{
-		if (PE_parse_boot_argn(bootargPatchPCIWithList, ignored_device_list, sizeof(ignored_device_list)))
-		{
-			DBGLOG("HBFX", "ignored device list=%s", ignored_device_list);
-			if (strstr(ignored_device_list, "none") != nullptr ||
-				strstr(ignored_device_list, "false") != nullptr ||
-				strstr(ignored_device_list, "off") != nullptr)
-			{
-				patchPCIFamily = false;
-				DBGLOG("HBFX", "Turn off PCIFamily patching since %s contains none, false or off", bootargPatchPCIWithList);
-			}
-			else
-				DBGLOG("HBFX", "boot-arg %s specified, turn on PCIFamily patching", bootargPatchPCIWithList);
-		}
-		
-		if (checkKernelArgument(bootargDisablePatchPCI))
-		{
-			patchPCIFamily = false;
-			DBGLOG("HBFX", "boot-arg %s specified, turn off PCIFamily patching", bootargDisablePatchPCI);
-		}
-	}
-	
-	if (PE_parse_boot_argn(bootargAutoHibernateMode, &autoHibernateMode, sizeof(autoHibernateMode)))
-	{
-		DBGLOG("HBFX", "boot-arg %s specified, value is 0x%02x", bootargAutoHibernateMode, autoHibernateMode);
-	}
+    if (getKernelVersion() >= KernelVersion::Sierra)
+    {
+        if (PE_parse_boot_argn(bootargPatchPCIWithList, ignored_device_list, sizeof(ignored_device_list)))
+        {
+            DBGLOG("HBFX", "ignored device list=%s", ignored_device_list);
+            if (strstr(ignored_device_list, "none") != nullptr ||
+                strstr(ignored_device_list, "false") != nullptr ||
+                strstr(ignored_device_list, "off") != nullptr)
+            {
+                patchPCIFamily = false;
+                DBGLOG("HBFX", "Turn off PCIFamily patching since %s contains none, false, or off", bootargPatchPCIWithList);
+            }
+            else
+                DBGLOG("HBFX", "boot-arg %s specified, turn on PCIFamily patching", bootargPatchPCIWithList);
+        }
+
+        if (checkKernelArgument(bootargDisablePatchPCI))
+        {
+            patchPCIFamily = false;
+            DBGLOG("HBFX", "boot-arg %s specified, turn off PCIFamily patching", bootargDisablePatchPCI);
+        }
+    }
+
+    if (PE_parse_boot_argn(bootargAutoHibernateMode, &autoHibernateMode, sizeof(autoHibernateMode)))
+    {
+        DBGLOG("HBFX", "boot-arg %s specified, value is 0x%02x", bootargAutoHibernateMode, autoHibernateMode);
+    }
 }
 
 PluginConfiguration ADDPR(config) {
-	xStringify(PRODUCT_NAME),
-	parseModuleVersion(xStringify(MODULE_VERSION)),
-	LiluAPI::AllowNormal,
-	ADDPR(hbfx_config).bootargOff,
-	arrsize(ADDPR(hbfx_config).bootargOff),
-	ADDPR(hbfx_config).bootargDebug,
-	arrsize(ADDPR(hbfx_config).bootargDebug),
-	ADDPR(hbfx_config).bootargBeta,
-	arrsize(ADDPR(hbfx_config).bootargBeta),
-	KernelVersion::MountainLion,
-	KernelVersion::Sonoma,
-	[]() {
-		ADDPR(hbfx_config).readArguments();
-		hbfx.init();
-	}
+    xStringify(PRODUCT_NAME),
+    parseModuleVersion(xStringify(MODULE_VERSION)),
+    LiluAPI::AllowNormal,
+    ADDPR(hbfx_config).bootargOff,
+    arrsize(ADDPR(hbfx_config).bootargOff),
+    ADDPR(hbfx_config).bootargDebug,
+    arrsize(ADDPR(hbfx_config).bootargDebug),
+    ADDPR(hbfx_config).bootargBeta,
+    arrsize(ADDPR(hbfx_config).bootargBeta),
+    KernelVersion::MountainLion,
+    KernelVersion::Sonoma,
+    []() {
+        ADDPR(hbfx_config).readArguments();
+        hbfx.init();
+    }
 };
