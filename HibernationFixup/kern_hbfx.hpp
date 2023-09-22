55bf5088b33519eec89565d2c3edc2534ea05e83
amane1234 <47608969+amane1234@users.noreply.github.com>
2023-09-22 13:19:41 -0400

    Update kern_hbfx.hpp

--- HibernationFixup/kern_hbfx.hpp
+++ HibernationFixup/kern_hbfx.hpp
@@ -17,151 +17,93 @@
 
 class HBFX {
 public:
-	bool init();
-	void deinit();
-	
+    bool init();
+    void deinit();
+
 private:
-	/**
-	 *  Patch kernel
-	 *
-	 *  @param patcher KernelPatcher instance
-	 */
-	void processKernel(KernelPatcher &patcher);
-	
-	/**
-	 *  Patch kext if needed and prepare other patches
-	 *
-	 *  @param patcher KernelPatcher instance
-	 *  @param index   kinfo handle
-	 *  @param address kinfo load address
-	 *  @param size    kinfo memory size
-	 */
-	void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
-	
-	/**
-	 *  Check the second RTC memory bank availability
-	 */
-	bool checkRTCExtendedMemory();
-	
-	/**
-	 *  Initialize NVStorage
-	 *
-	 */
-	bool initializeNVStorage();
-	
-	// read supported options from NVRAM
-	void readConfigFromNVRAM();
-	
-	// return pointer to IOPMPowerSource
-	IOPMPowerSource *getPowerSource();
-	
-	// return true if standby/auto power off is enabled
-	bool isStandbyEnabled(uint32_t &standby_delay, bool &pmset_default_mode);
-	
-	IOReturn explicitlyCallSetMaintenanceWakeCalendar();
-	
-	void checkCapacity();
-	
-	/**
-	 *  Hooked methods / callbacks
-	 */
-	static IOReturn     IOHibernateSystemSleep(void);
-	static IOReturn     IOHibernateSystemWake(void);
-	
-	static void         IOPMrootDomain_evaluatePolicy(IOPMrootDomain* that, int stimulus, uint32_t arg);
-	static void         IOPMrootDomain_requestFullWake(IOPMrootDomain* that, uint32_t reason);
-	static IOReturn     IOPMrootDomain_setMaintenanceWakeCalendar(IOPMrootDomain* that, IOPMCalendarStruct * calendar);
-	static IOReturn     AppleRTC_setupDateTimeAlarm(void *that, void* rctDateTime);
-	static IOReturn     X86PlatformPlugin_sleepPolicyHandler(void * target, IOPMSystemSleepPolicyVariables * vars, IOPMSystemSleepParameters * params);
-	
-	static int          packA(char *inbuf, uint32_t length, uint32_t buflen);
-	static IOReturn     IOPCIBridge_restoreMachineState(IOService *that, IOOptionBits options, IOService * device);
-	static void         IOPCIDevice_extendedConfigWrite16(IOService *that, UInt64 offset, UInt16 data);
-	
-	
-	/**
-	 *  Trampolines for original method invocations
-	 */
-	mach_vm_address_t orgIOHibernateSystemSleep {};
-	mach_vm_address_t orgIOHibernateSystemWake {};
-	
-	mach_vm_address_t orgIOPMrootDomain_evaluatePolicy {};
-	mach_vm_address_t orgIOPMrootDomain_requestFullWake {};
-	mach_vm_address_t orgIOPMrootDomain_setMaintenanceWakeCalendar {};
-	mach_vm_address_t orgX86PlatformPlugin_sleepPolicyHandler {};
-	mach_vm_address_t orgAppleRTC_setupDateTimeAlarm {};
-	mach_vm_address_t orgPackA {};
-	mach_vm_address_t orgIOPCIBridge_restoreMachineState {};
-	mach_vm_address_t orgIOPCIDevice_extendedConfigWrite16 {};
-	
-	/**
-	 *  Sync file buffers & interrupts & preemption control
-	 */
-	using t_sync = int (*) (__unused proc_t p, __unused struct sync_args *uap, __unused int32_t *retval);
-	t_sync sync {nullptr};
-	
-	using t_preemption_enabled = boolean_t (*) (void);
-	t_preemption_enabled preemption_enabled {nullptr};
-	
-	using t_enable_preemption = void (*) (void);
-	t_enable_preemption enable_preemption {nullptr};
-	
-	using t_disable_preemption = void (*) (void);
-	t_disable_preemption disable_preemption {nullptr};
-	
-	using t_ml_at_interrupt_context = boolean_t (*) (void);
-	t_ml_at_interrupt_context ml_at_interrupt_context {nullptr};
-	
-	using t_ml_get_interrupts_enabled = boolean_t (*) (void);
-	t_ml_get_interrupts_enabled ml_get_interrupts_enabled {nullptr};
-	
-	using t_ml_set_interrupts_enabled = boolean_t (*) (boolean_t enable);
-	t_ml_set_interrupts_enabled ml_set_interrupts_enabled {nullptr};
-	
-	using t_convertDateTimeToSeconds = int64_t (*) (void *rctDateTime);
-	t_convertDateTimeToSeconds convertDateTimeToSeconds {nullptr};
-	
-	using t_convertSecondsToDateTime = int64_t (*) (int64_t seconds, void *rctDateTime);
-	t_convertSecondsToDateTime convertSecondsToDateTime {nullptr};
-	
-	using t_checkSystemSleepEnabled = bool (*) (IOPMrootDomain* that);
-	t_checkSystemSleepEnabled checkSystemSleepEnabled {nullptr};
-	
-	bool    correct_pci_config_command {false};
-	
-	uint32_t  	latestStandbyDelay {0};
-	uint32_t    latestPoweroffDelay {0};
-	uint32_t  	latestHibernateMode {0};
-	uint32_t  	sleepPhase {-1U};
-	uint64_t    sleepFactors {0};
-	uint32_t    sleepReason {0};
-	uint32_t    sleepType {0};
-	uint32_t    sleepFlags {0};
-	bool	  	sleepServiceWake {false};
-	bool	  	wakeCalendarSet {false};
-	
-	/**
-	 *  Current progress mask
-	 */
-	struct ProcessingState {
-		enum {
-			NothingReady = 0,
-			IOPCIFamilyRouted = 1,
-			KernelRouted = 2,
-			AppleRTCRouted = 4,
-			X86PluginRouted = 8,
-			EverythingDone = IOPCIFamilyRouted | KernelRouted | AppleRTCRouted | X86PluginRouted,
-		};
-	};
-	int progressState {ProcessingState::NothingReady};
-	
-	NVStorage nvstorage;
-	IOWorkLoop *workLoop {};
-	IOTimerEventSource *nextSleepTimer {};
-	IOTimerEventSource *checkCapacityTimer {};
-	bool emulatedNVRAM {false};
+    // Private methods
+
+    void processKernel(KernelPatcher &patcher);
+    void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
+    bool checkRTCExtendedMemory();
+    bool initializeNVStorage();
+    void readConfigFromNVRAM();
+    IOPMPowerSource *getPowerSource();
+    bool isStandbyEnabled(uint32_t &standby_delay, bool &pmset_default_mode);
+    IOReturn explicitlyCallSetMaintenanceWakeCalendar();
+    void checkCapacity();
+
+    // Hooked methods / callbacks
+
+    static IOReturn IOHibernateSystemSleep(void);
+    static IOReturn IOHibernateSystemWake(void);
+    static void IOPMrootDomain_evaluatePolicy(IOPMrootDomain* that, int stimulus, uint32_t arg);
+    static void IOPMrootDomain_requestFullWake(IOPMrootDomain* that, uint32_t reason);
+    static IOReturn IOPMrootDomain_setMaintenanceWakeCalendar(IOPMrootDomain* that, IOPMCalendarStruct * calendar);
+    static IOReturn AppleRTC_setupDateTimeAlarm(void *that, void* rctDateTime);
+    static IOReturn X86PlatformPlugin_sleepPolicyHandler(void * target, IOPMSystemSleepPolicyVariables * vars, IOPMSystemSleepParameters * params);
+    static int packA(char *inbuf, uint32_t length, uint32_t buflen);
+    static IOReturn IOPCIBridge_restoreMachineState(IOService *that, IOOptionBits options, IOService * device);
+    static void IOPCIDevice_extendedConfigWrite16(IOService *that, UInt64 offset, UInt16 data);
+
+    // Trampolines for original method invocations
+
+    mach_vm_address_t orgIOHibernateSystemSleep {};
+    mach_vm_address_t orgIOHibernateSystemWake {};
+    mach_vm_address_t orgIOPMrootDomain_evaluatePolicy {};
+    mach_vm_address_t orgIOPMrootDomain_requestFullWake {};
+    mach_vm_address_t orgIOPMrootDomain_setMaintenanceWakeCalendar {};
+    mach_vm_address_t orgX86PlatformPlugin_sleepPolicyHandler {};
+    mach_vm_address_t orgAppleRTC_setupDateTimeAlarm {};
+    mach_vm_address_t orgPackA {};
+    mach_vm_address_t orgIOPCIBridge_restoreMachineState {};
+    mach_vm_address_t orgIOPCIDevice_extendedConfigWrite16 {};
+
+    // Sync-related function pointers
+
+    using t_sync = int (*) (__unused proc_t p, __unused struct sync_args *uap, __unused int32_t *retval);
+    t_sync sync {nullptr};
+    using t_preemption_enabled = boolean_t (*) (void);
+    t_preemption_enabled preemption_enabled {nullptr};
+    using t_enable_preemption = void (*) (void);
+    t_enable_preemption enable_preemption {nullptr};
+    using t_disable_preemption = void (*) (void);
+    t_disable_preemption disable_preemption {nullptr};
+    using t_ml_at_interrupt_context = boolean_t (*) (void);
+    t_ml_at_interrupt_context ml_at_interrupt_context {nullptr};
+    using t_ml_get_interrupts_enabled = boolean_t (*) (void);
+    t_ml_get_interrupts_enabled ml_get_interrupts_enabled {nullptr};
+    using t_ml_set_interrupts_enabled = boolean_t (*) (boolean_t enable);
+    t_ml_set_interrupts_enabled ml_set_interrupts_enabled {nullptr};
+    using t_convertDateTimeToSeconds = int64_t (*) (void *rctDateTime);
+    t_convertDateTimeToSeconds convertDateTimeToSeconds {nullptr};
+    using t_convertSecondsToDateTime = int64_t (*) (int64_t seconds, void *rctDateTime);
+    t_convertSecondsToDateTime convertSecondsToDateTime {nullptr};
+    using t_checkSystemSleepEnabled = bool (*) (IOPMrootDomain* that);
+    t_checkSystemSleepEnabled checkSystemSleepEnabled {nullptr};
+
+    // Other member variables
+
+    bool correct_pci_config_command {false};
+    uint32_t latestStandbyDelay {0};
+    uint32_t latestPoweroffDelay {0};
+    uint32_t latestHibernateMode {0};
+    uint32_t sleepPhase {-1U};
+    uint64_t sleepFactors {0};
+    uint32_t sleepReason {0};
+    uint32_t sleepType {0};
+    uint32_t sleepFlags {0};
+    bool sleepServiceWake {false};
+    bool wakeCalendarSet {false};
+    int progressState {ProcessingState::NothingReady};
+    NVStorage nvstorage;
+    IOWorkLoop *workLoop {};
+    IOTimerEventSource *nextSleepTimer {};
+    IOTimerEventSource *checkCapacityTimer {};
+    bool emulatedNVRAM {false};
+
 #ifdef DEBUG
-	int lastStimulus {};
+    int lastStimulus {};
 #endif
 };
 
