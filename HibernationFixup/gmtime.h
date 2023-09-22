7e20e944812eece67b0d4b65dda26119dabe6c63
amane1234 <47608969+amane1234@users.noreply.github.com>
2023-09-22 13:08:18 -0400

    Update gmtime.h

--- HibernationFixup/gmtime.h
+++ HibernationFixup/gmtime.h
@@ -1,34 +1,20 @@
-//
-//  gmtime.h
-//  HibernationFixup
-//
-//  Copyright © 2019 lvs1974. All rights reserved.
-//
-
 #ifndef gmtime_h
 #define gmtime_h
 
+#include <time.h> // Include time.h for 'time_t' type
+
 struct tm {
-	int 	tm_sec;
-	// seconds after the minute - [0,59]
-	int 	tm_min;
-	// minutes after the hour - [0,59]
-	int 	tm_hour;
-	// hours since midnight - [0,23]
-	int 	tm_mday;
-	// day of the month - [1,31]
-	int 	tm_mon;
-	// months since January - [0,11]
-	int 	tm_year;
-	// years since 1900
-	int 	tm_wday;
-	// days since Sunday - [0,6]
-	int 	tm_yday;
-	// days since January 1 - [0,365]
-	int 	tm_isdst;
-	// daylight savings time flag
+    int tm_sec;    // seconds after the minute - [0,59]
+    int tm_min;    // minutes after the hour - [0,59]
+    int tm_hour;   // hours since midnight - [0,23]
+    int tm_mday;   // day of the month - [1,31]
+    int tm_mon;    // months since January - [0,11]
+    int tm_year;   // years since 1900
+    int tm_wday;   // days since Sunday - [0,6]
+    int tm_yday;   // days since January 1 - [0,365]
+    int tm_isdst;  // daylight savings time flag
 };
 
-struct tm * gmtime_r(time_t timer, struct tm * timeptr);
+struct tm *gmtime_r(time_t timer, struct tm *timeptr);
 
-#endif /* gmtime_h */
+#endif 
